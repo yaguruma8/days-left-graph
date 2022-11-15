443b00019b96e058521e23948f18e73a9a3aa13f
yaguruma <yaguruma_y@yahoo.co.jp>
2022-11-16 06:43:59 +0900

    [add] グラフデータの作成を追加

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-console.log('hello!');
+'use strict';
+// 定数
+// グラフの色
+const BG_COLOR_PAST = 'lightgray';
+const BG_COLOR_LEFT = 'pink';
 
 // 要素の取得
 const thisYearElement = document.querySelector('#thisyear');
@@ -8,29 +12,66 @@ const leftDaysElement = document.querySelector('#leftdays');
 // 日付の計算
 const now = new Date();
 const thisYear = now.getFullYear();
+const thisYearDays = getThisYearDays(isLeapYear(now));
 // 経過日数を計算
 const pastDays = getPastDays(now);
 // 残り日数を計算
-const leftDays = isLeapYear(now) ? 366 - pastDays : 365 - pastDays;
-console.log(pastDays, leftDays);
+const leftDays = thisYearDays - pastDays;
+// 残り時間を計算
+// todo: 0:00の時に23時間60分になるので修正考えること
+const leftHours = 23 - now.getHours();
+const leftMinutes = 60 - now.getMinutes();
+const leftTimeString = `${leftHours} 時間 ${leftMinutes} 分`
+// 経過％を計算
+const pastPar = ((pastDays / thisYearDays) * 100).toFixed(1);
+// 残り％を計算
+const leftPar = (100 - Number(pastPar)).toFixed(1);
+console.log(pastDays, leftDays, pastPar, leftPar);
 
 // 要素のtextContentに表示
 thisYearElement.textContent = String(thisYear);
-pastDaysElement.textContent = String(pastDays);
-leftDaysElement.textContent = String(leftDays);
+pastDaysElement.textContent = `${String(pastDays)} 日 (${pastPar}%)`;
+leftDaysElement.textContent = `${String(
+  leftDays
+)} 日 と ${leftTimeString} (${leftPar}%)`;
 
 // グラフの表示 (chart.js)
 // データの作成
 let labels = [];
 let data = [];
+let backgroundColor = [];
+let past = pastDays;
 for (let i = 1; i <= 12; i++) {
-  labels.push(`${i}月`);
-  labels.push('');
-  data.push(new Date(thisYear, i, 0).getDate());
-  data.push(0);
+  const thisMonthDays = new Date(thisYear, i, 0).getDate();
+  // pastが残り0日以下 = left
+  // pastが残り0日より大きく、thisMonthDays以下 = pastとleft
+  // pastがthisMonthDaysより大きい = past
+  if (past > thisMonthDays) {
+    labels.push(`${i}月`);
+    labels.push('');
+    data.push(thisMonthDays);
+    data.push(0);
+    backgroundColor.push(BG_COLOR_PAST);
+    backgroundColor.push(BG_COLOR_PAST);
+  } else if (past > 0 && past <= thisMonthDays) {
+    labels.push(`${i}月`);
+    labels.push(`${i}月`);
+    data.push(past);
+    data.push(thisMonthDays - past);
+    backgroundColor.push(BG_COLOR_PAST);
+    backgroundColor.push(BG_COLOR_LEFT);
+  } else {
+    labels.push('');
+    labels.push(`${i}月`);
+    data.push(0);
+    data.push(thisMonthDays);
+    backgroundColor.push(BG_COLOR_LEFT);
+    backgroundColor.push(BG_COLOR_LEFT);
+  }
+  past -= thisMonthDays;
 }
 
-const ctx = document.getElementById('myChart');
+const ctx = document.getElementById('chart');
 new Chart(ctx, {
   type: 'doughnut',
   data: {
@@ -39,13 +80,12 @@ new Chart(ctx, {
       {
         label: 'days left in the year',
         data: data,
-        backgroundColor: ['lightgray'],
+        backgroundColor: backgroundColor,
         hoverOffset: 0,
       },
     ],
   },
   options: {
-    // Add plugins to options.
     plugins: {
       legend: {
         // グラフのラベルを非表示にする（hoverでは表示される)
@@ -53,17 +93,6 @@ new Chart(ctx, {
       },
     },
   },
-  // options: {
-  //   animations: {
-  //     tension: {
-  //       duration: 1000,
-  //       easing: 'linear',
-  //       from: 1,
-  //       to: 0,
-  //       loop: true
-  //     }
-  //   },
-  // }
 });
 
 // 一年の経過日数を返す
@@ -83,4 +112,9 @@ function getPastDays(date) {
 function isLeapYear(date) {
   const year = date.getFullYear();
   return new Date(year, 2, 0).getDate() === 29;
+}
+
+// その年の一年の日数を返す
+function getThisYearDays(isLeapYear) {
+  return isLeapYear ? 366 : 365;
 }
